8059cbad023ebe5ca25071c565f6c3c1d99d1eb2
jespino2smu <hed-jespino@smu.edu.ph>
2026-04-09 01:17:53 +0800

    Removed unnecessary files.

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -21,7 +21,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-
 proxy.on('error', (err, req, res) => {
   res.writeHead(500, { 'Content-Type': 'text/plain' });
   res.end('Proxy error: Connection refused.');
